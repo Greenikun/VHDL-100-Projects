b97405d479f3b6b1e58b3d65b72d0998944a8eb7
TheChipMaker <a.nabil0007@gmail.com>
2025-09-07 17:36:41 +0300

    Fixing the testbench

--- Stage 1 - Combinational Basics (no clock yet)/21_HalfAdder/HalfAdder_tb.vhd
+++ Stage 1 - Combinational Basics (no clock yet)/21_HalfAdder/HalfAdder_tb.vhd
@@ -5,7 +5,7 @@ ENTITY HalfAdder_tb IS
 END HalfAdder_tb;
 
 ARCHITECTURE Test OF HalfAdder_tb IS
-    SIGNAL A, B, S, C : STD_LOGIC; -- Signals to connect to HalfAdder
+    SIGNAL A, B, S, C : STD_LOGIC;  -- Signals to connect to HalfAdder
 BEGIN
     DuT : ENTITY work.HalfAdder
         PORT MAP(
@@ -19,33 +19,33 @@ BEGIN
         VARIABLE expected_S, expected_C : STD_LOGIC; -- Used for self-checking outputs
     BEGIN
         -- Test 0 + 0
-        A <= '0';
-        B <= '0';
-        expected_S := '0';
-        expected_C := '0';
+        A <= '0'; B <= '0';
+        expected_S := '0'; expected_C := '0';
         WAIT FOR 1 ns;
         ASSERT (expected_S = S AND expected_C = C)
-        REPORT "Error: Half Adder failed for A=0 B=0" SEVERITY ERROR;
+            REPORT "Error: Half Adder failed for A=0 B=0" SEVERITY ERROR;
 
         -- Test 0 + 1
-        A <= '0';
-        B <= '1';
-        expected_S := '1';
-        expected_C := '0';
+        A <= '0'; B <= '1';
+        expected_S := '1'; expected_C := '0';
         WAIT FOR 1 ns;
         ASSERT (expected_S = S AND expected_C = C)
-        REPORT "Error: Half Adder failed for A=0 B=1" SEVERITY ERROR;
+            REPORT "Error: Half Adder failed for A=0 B=1" SEVERITY ERROR;
 
         -- Test 1 + 0
-        A <= '1';
-        B <= '0';
-        expected_S := '1';
-        expected_C := '0';
+        A <= '1'; B <= '0';
+        expected_S := '1'; expected_C := '0';
         WAIT FOR 1 ns;
         ASSERT (expected_S = S AND expected_C = C)
-        REPORT "Error: Half Adder failed for A=1 B=0" SEVERITY ERROR;
+            REPORT "Error: Half Adder failed for A=1 B=0" SEVERITY ERROR;
 
         -- Test 1 + 1
-        A <= '1';
-        B <= '1';
-        expe
+        A <= '1'; B <= '1';
+        expected_S := '0'; expected_C := '1';
+        WAIT FOR 1 ns;
+        ASSERT (expected_S = S AND expected_C = C)
+            REPORT "Error: Half Adder failed for A=1 B=1" SEVERITY ERROR;
+
+        WAIT; -- End simulation
+    END PROCESS;
+END Test;
