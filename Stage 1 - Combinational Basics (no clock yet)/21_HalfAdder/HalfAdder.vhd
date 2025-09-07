3576e7f82fe6093a674dba59eb7a265a030fdc07
TheChipMaker <a.nabil0007@gmail.com>
2025-09-07 16:59:39 +0300

    Adding the HalfAdder Code

---
+++ Stage 1 - Combinational Basics (no clock yet)/21_HalfAdder/HalfAdder.vhd
@@ -0,0 +1,17 @@
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+
+ENTITY HalfAdder IS
+    PORT (
+        SIGNAL A : IN STD_LOGIC;
+        SIGNAL B : IN STD_LOGIC;
+        SIGNAL S : OUT STD_LOGIC;
+        SIGNAL C : OUT STD_LOGIC
+    );
+END HalfAdder;
+-----------------------------------------------
+ARCHITECTURE Behaviour OF HalfAdder IS
+BEGIN
+    S <= A XOR B;
+    C <= A AND B;
+END Behaviour;
