93fcb722e76b5e78cd2fba1bf2a332ddcb9a0d9c
TheChipMaker <a.nabil0007@gmail.com>
2025-09-08 11:45:45 +0300

    Pushing the full adder tb initial code

--- Stage 1 - Combinational Basics (no clock yet)/22_FullAdder/FullAdder_tb.vhd
+++ Stage 1 - Combinational Basics (no clock yet)/22_FullAdder/FullAdder_tb.vhd
@@ -9,5 +9,31 @@ END FullAdder_tb;
 --
 --
 ARCHITECTURE Test OF FullAdder_tb IS
+    SIGNAL A : STD_LOGIC := '0';
+    SIGNAL B : STD_LOGIC := '0';
+    SIGNAL Cin : STD_LOGIC := '0';
+    SIGNAL Sum : STD_LOGIC;
+    SIGNAL Cout : STD_LOGIC;
 BEGIN
+
+    DuT : ENTITY work.FullAdder
+        PORT MAP(
+            A => A,
+            B => B,
+            Cin => Cin,
+            Sum => Sum,
+            Cout => Cout
+        );
+    stim_proc : PROCESS
+        VARIABLE vec : STD_LOGIC_VECTOR(2 DOWNTO 0);
+    BEGIN
+        FOR i IN 0 TO 7 LOOP
+            vec := STD_LOGIC_VECTOR(to_unsigned(i, 3));
+            A <= vec(2);
+            B <= vec(1);
+            Cin <= vec(0);
+            WAIT FOR 1 ns;
+        END LOOP;
+        WAIT;
+    END stim_proc PROCESS;
 END Test;
